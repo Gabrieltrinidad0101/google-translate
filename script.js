54df65f8c6c15d2cd3cd0ac308ac299ec443186c
gabriel <gabrielqwes123@gmil.com>
2022-06-13 01:20:41 -0400

    youtube version

--- script.js
+++ script.js
@@ -1,66 +1,59 @@
-import countries from "./constant.js" 
+import languages from "./languages.js";
+
 const selectFirst = document.querySelector(".first")
-const selectSecond = document.querySelector(".second") 
-const translate = document.querySelector(".translate") 
-const change = document.getElementById("change")
+const selectSecond = document.querySelector(".second")
+const translate = document.querySelector(".translate")
 const fromText = document.querySelector(".fromText")
 const toText = document.querySelector(".toText")
+const change =  document.getElementById("change")
+const reades = document.querySelectorAll(".read")
 const listen = document.querySelector(".listen")
-const reads = document.querySelectorAll(".read")
+const language1 = "en-GB"
+const language2 = "es-ES"
 
-let language1 = "en-GB"
-let language2 =  "es-ES"
-
-
-for (const i in countries){
-  const key = Object.keys(countries[i]).toString()
-  const value = Object.values(countries[i]).toString()
-  selectFirst.innerHTML += `<option value="${key}">${value}</option>`
-  selectSecond.innerHTML += `<option value="${key}">${value}</option>`
+for(const i in languages){
+    const key = Object.keys(languages[i]).toString()
+    const value = Object.values(languages[i]).toString()
+    selectFirst.innerHTML += `<option value=${key}>${value}</option>`
+    selectSecond.innerHTML += `<option value=${key}>${value}</option>`
 }
 
 selectFirst.value = language1
 selectSecond.value = language2
 
-
 change.addEventListener("click",_=>{
-  const selectSecondValue = selectFirst.value
-  selectFirst.value = selectSecond.value
-  selectSecond.value = selectSecondValue
+    const selectFirstValue = selectFirst.value
+    selectFirst.value = selectSecond.value
+    selectSecond.value = selectFirstValue
 
-  if(!toText.value) return
-  const fromTextValue = fromText.value
-  fromText.value = toText.value
-  toText.value = fromTextValue
+    if(!toText.value) return
+    const fromTextValue = fromText.value
+    fromText.value = toText.value
+    toText.value = fromTextValue
 })
 
 translate.addEventListener("click",async _=>{
-  if(!fromText.value) return
-  const res = await fetch(`https://api.mymemory.translated.net/get?q=${fromText.value}&langpair=${selectFirst.value}|${selectSecond.value}`)
-  const data = await res.json()
-  toText.value = data.responseData.translatedText
+    if(!fromText.value) return
+    const res = await fetch(`https://api.mymemory.translated.net/get?q=${fromText.value}&langpair=${selectFirst.value}|${selectSecond.value}`)
+    const data = await res.json()
+    toText.value = data.responseData.translatedText
 })
 
-reads.forEach((read,index)=>{
-  read.addEventListener("click",()=>{
-    const textToRead = index == 0 ? fromText.value : toText.value
-    if(!textToRead) return
-    speechSynthesis.speak(new SpeechSynthesisUtterance(textToRead))
-  })
-})
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-var recognition = new SpeechRecognition();
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+const recognition = new SpeechRecognition()
 
-recognition.onresult = function(event) {
-  fromText.value = event.results[0][0].transcript
+reades.forEach((read,index)=>{
+    read.addEventListener("click",_=>{
+        const textToRead = index == 0 ? fromText.value : toText.value
+        if(!textToRead) return
+        speechSynthesis.speak(new SpeechSynthesisUtterance(textToRead))
+    })
+});
+
+recognition.onresult = (event)=>{
+    fromText.value = event.results[0][0].transcript
 }
-
-recognition.onerror = function(event) {
-  console.log(event)
-}
-
 
 listen.addEventListener("click",_=>{
     recognition.start()
 })
-          
